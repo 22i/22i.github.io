3e315401a8a7945f611723ea439e3f947b8da00f
tri <pumpkin650@gmail.com>
2020-11-11 08:56:03 +0100

    update

--- content/scripts/generate_name/3_censorship.sh
+++ content/scripts/generate_name/3_censorship.sh
@@ -5,7 +5,13 @@
 # cp all_the_names all_the_names1
 
 # remove some names that match from the remove text file in the lib folder
-grep -Fvxf $PWD/lib/remove all_the_names > all_the_names1
+# grep -Fvxf $PWD/lib/remove all_the_names > all_the_names1
+
+
+sed -i '/emo/d' all_the_names
+sed -i '/Emo/d' all_the_names
+
+
 
 # rm -f all_the_names1
 
--- content/scripts/update_website/update_website.sh
+++ content/scripts/update_website/update_website.sh
@@ -12,29 +12,41 @@
 
 # UPDATE THE FAVICON WEBSITE ICON
 
-# go inside lib folder
+# go inside lib folder then lib folder then icons folder then solid folder
+# content/scripts/update_website/
 cd $PWD/lib/icons/solid
+# content/scripts/update_website/lib/icons/solid
 
-
-
+# remove everything inside from folder 2 that is ../ up a directory
 rm --force --recursive ../2/*
 
+# remove everything inside from folder 3 that is ../ up a directory
 rm --force --recursive ../3/*
 
 
 
-# to make it faster copy 1 random file from solid
+# to make it faster copy 1 random file from solid into folder 3 that is up a directory
 # https://unix.stackexchange.com/questions/217712/randomly-copy-certain-amount-of-certain-file-type-from-one-directory-into-anothe#217720
 shuf -zn1 -e *.svg | xargs -0 cp -t ../3/
 
+favicon=`ls ../3`
+
+
+# go up a directory and into folder 3
+# content/scripts/update_website/lib/icons/solid
 cd ../3/
+# content/scripts/update_website/lib/icons/3
 
 # if there is no style then add it
 # style="fill:white;"
-# remove 4 bytes from beggining
-# dd bs=1131 skip=1 if=filtered.dump of=trimmed.dump
-for i in *; do
-tail -c +5 $i > ../2/$i; done
+# remove 4 bytes from beggining or 4 characters or just <svg
+# for i in *; do
+# tail -c +5 $i > ../2/$i; done
+tail -c +5 *.svg > ../2/icon.svg
+
+
+
+
 
 # if there is no style then add it
 # style="fill:white;"
@@ -56,12 +68,14 @@ tail -c +5 $i > ../2/$i; done
 
 # add svg color to begining of the file
 # <svg
+# go up a folder and into folder 2
 cd ../2/
-for i in *; do
-# sed -i '1i <svg style="fill:white;"' $i; done
+# for i in *; do
+# put a random line from colors text file that is 2 folders up into variable color
 color=`shuf -n 1 ../../colors`
-sed -i '1i <svg style="fill:'$color';"' $i; done
-
+# add text inside the svg with variable color
+# sed -i '1i <svg style="fill:'$color';"' $i; done
+sed -i '1i <svg style="fill:'$color';"' icon.svg
 
 # add svg color to begining of the file
 # <svg
@@ -76,19 +90,21 @@ sed -i '1i <svg style="fill:'$color';"' $i; done
 # <svg
 # sed -i '1i <svg style="fill:white;"' amazing.svg
 
-
+# go up a folder
+# content/scripts/update_website/lib/icons/3
 cd ../
+# content/scripts/update_website/lib/icons/
 
 # ls 3 > ../1.list
 
 
 # favicon=`shuf -n 1 ../1.list`
 
-favicon=`ls 2`
+# favicon=`ls 2`
 
-
+# content/scripts/update_website/lib/icons/
 cd ../../
-
+# content/scripts/update_website/
 
 # get random line from all_the_names and put it into variable favicon
 # https://stackoverflow.com/questions/448005/whats-an-easy-way-to-read-random-line-from-a-file-in-unix-command-line
@@ -100,18 +116,24 @@ cd ../../
 
 # searches 3 folders up in the index.html text for line with amazing8543G345IrZ73Z22i then replaces it with <!--amazing8543G345IrZ73Z22i--><title>$name</title>"
 # https://www.golinuxhub.com/2017/06/sed-replace-whole-line-when-match-found/
-sed -i '/amazing8543G345IrZ73Z22i/c<!--amazing8543G345IrZ73Z22i--><link rel="icon" sizes="any" type="image/svg+xml" href="content/scripts/update_website/lib/icons/2/'$favicon'">' ../../../index.html
+# sed -i '/amazing8543G345IrZ73Z22i/c<!--amazing8543G345IrZ73Z22i--><link rel="icon" sizes="any" type="image/svg+xml" href="content/scripts/update_website/lib/icons/2/'$favicon'">' ../../../index.html
+
+# searches 3 folders up in the index.html text for line with amazing8543G345IrZ73Z22i then replaces it with <!--amazing85ghfzu6ud573Z22i--><title>$name</title>"
+# https://www.golinuxhub.com/2017/06/sed-replace-whole-line-when-match-found/
+sed -i '/amazing85ghfzu6ud573Z22i/c<!--amazing85ghfzu6ud573Z22i--><!--'$color $favicon'-->' ../../../index.html
 
 # prints in the terminal
 # echo website name in index.html at line 10 changed to $name
 echo changed website favicon to: $color $favicon
 
-rm --force --recursive ../3/*
+rm --force --recursive $PWD/lib/icons/3/*
 
 
 
 # change website name to random video game name
+# content/scripts/update_website/
 cd ../generate_name/
+# content/scripts/generate_name/
 # sh 3_change_website_name.sh
 
 # get random line from all_the_names and put it into variable name
@@ -131,12 +153,16 @@ sed -i "/amazing63z947ziT9Eas722i/c<!--amazing63z947ziT9Eas722i--><title>$name</
 echo changed website name to: $name
 
 # update the website preview
+# content/scripts/generate_name/
 cd ../website_preview
+# content/scripts/website_preview/
 sh preview.sh
 echo website preview updated for the readme
 
 # update the website update version
+# content/scripts/website_preview/
 cd ../update_website/
+# content/scripts/update_website/
 
 # get line 95 into variable and keep only numbers
 # goes up 3 folders
