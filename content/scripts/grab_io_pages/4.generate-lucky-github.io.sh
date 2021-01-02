df0633ec0f70c4d75850640976b89cf8a3e197f8
tri <pumpkin650@gmail.com>
2021-01-02 21:36:16 +0100

    our favorite button is working again it just needed information about number of links inside masterlist.txt

--- content/scripts/grab_io_pages/4.generate-lucky-github.io.sh
+++ content/scripts/grab_io_pages/4.generate-lucky-github.io.sh
@@ -5,7 +5,8 @@
 # because it recreates and reorders lucky.github.io.page.html
 
 
-
+# count how many lines then strip anything besides numbers put it into variable iold_number
+number_of_links_in_masterlist=`wc -l masterlist.txt | sed 's/[^0-9]*//g'`
 
 
 # put prefix https://
@@ -17,8 +18,8 @@ awk '{ print "\""$0"\""}' 1 > 2
 # add a suffix ;
 awk 'NF{print $0 ";"}' 2 > 3
 
-# makes an array of starting from number 0 til (number depends on the variable) $inumber_of_links_in_masterlist
-seq -f "urls[%.0f]=" 0 $inumber_of_links_in_masterlist | sed '$ d' > aray
+# makes an array of starting from number 0 til (number depends on the variable) $number_of_links_in_masterlist
+seq -f "urls[%.0f]=" 0 $number_of_links_in_masterlist | sed '$ d' > aray
 
 # echo done aray
 
