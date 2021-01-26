882ba8b3e354d1c082382bc1cad8548a21c62824
tri <pumpkin650@gmail.com>
2021-01-26 07:36:07 +0100

    update

--- content/scripts/generate_name/3_censorship.sh
+++ content/scripts/generate_name/3_censorship.sh
@@ -1,133 +1,49 @@
 #!/bin/bash
 
-# rm -f all_the_names1
 
-# cp all_the_names all_the_names1
 
-# remove some names that match from the remove text file in the lib folder
-# grep -Fvxf $PWD/lib/remove all_the_names > all_the_names1
+rm -f 1 2 suffix prefix
 
-# removes all the names that include words listed here
-sed -i '/emo/d' all_the_names
-sed -i '/Emo/d' all_the_names
-sed -i '/Teenage/d' all_the_names
-sed -i '/teenage/d' all_the_names
-sed -i '/angry/d' all_the_names
-sed -i '/Angry/d' all_the_names
-sed -i '/prison/d' all_the_names
-sed -i '/Prison/d' all_the_names
-sed -i '/Killer/d' all_the_names
-sed -i '/killer/d' all_the_names
-sed -i '/Kiler/d' all_the_names
-sed -i '/kiler/d' all_the_names
-sed -i '/boring/d' all_the_names
-sed -i '/Boring/d' all_the_names
-sed -i '/scam/d' all_the_names
-sed -i '/Scam/d' all_the_names
-sed -i '/Big/d' all_the_names
-sed -i '/big/d' all_the_names
-sed -i '/small/d' all_the_names
-sed -i '/Small/d' all_the_names
-sed -i '/assault/d' all_the_names
-sed -i '/Assault/d' all_the_names
-sed -i '/asault/d' all_the_names
-sed -i '/Asault/d' all_the_names
-sed -i '/Toxic/d' all_the_names
-sed -i '/toxic/d' all_the_names
-sed -i '/african/d' all_the_names
-sed -i '/African/d' all_the_names
-sed -i '/Australian/d' all_the_names
-sed -i '/australian/d' all_the_names
-sed -i '/indian/d' all_the_names
-sed -i '/Indian/d' all_the_names
-sed -i '/gun/d' all_the_names
-sed -i '/Gun/d' all_the_names
-sed -i '/rugby/d' all_the_names
-sed -i '/Rugby/d' all_the_names
-sed -i '/Unremarkable/d' all_the_names
-sed -i '/unremarkable/d' all_the_names
-sed -i '/Equestrian/d' all_the_names
-sed -i '/equestrian/d' all_the_names
-sed -i '/jew/d' all_the_names
-sed -i '/Jew/d' all_the_names
-sed -i '/Jewish/d' all_the_names
-sed -i '/jewish/d' all_the_names
-sed -i '/In Your Face/d' all_the_names
-sed -i '/Incomprehensible/d' all_the_names
-sed -i '/incomprehensible/d' all_the_names
-sed -i '/Murder/d' all_the_names
-sed -i '/murder/d' all_the_names
-sed -i '/hitman/d' all_the_names
-sed -i '/Hitman/d' all_the_names
-sed -i '/Shopping/d' all_the_names
-sed -i '/shopping/d' all_the_names
-sed -i '/Globetrotters/d' all_the_names
-sed -i '/globetrotters/d' all_the_names
-sed -i '/Booty/d' all_the_names
-sed -i '/booty/d' all_the_names
-sed -i '/Cheese/d' all_the_names
-sed -i '/cheese/d' all_the_names
-sed -i '/Dollar/d' all_the_names
-sed -i '/dollar/d' all_the_names
-sed -i '/Blimp/d' all_the_names
-sed -i '/blimp/d' all_the_names
-sed -i '/Slaughter/d' all_the_names
-sed -i '/slaughter/d' all_the_names
-sed -i '/Dating/d' all_the_names
-sed -i '/dating/d' all_the_names
-sed -i '/Rodeo/d' all_the_names
-sed -i '/rodeo/d' all_the_names
-sed -i '/Donkey/d' all_the_names
-sed -i '/donkey/d' all_the_names
-sed -i '/DS/d' all_the_names
-sed -i '/Lawnmower/d' all_the_names
-sed -i '/lawnmower/d' all_the_names
-sed -i '/Crossdressing/d' all_the_names
-sed -i '/crossdressing/d' all_the_names
-sed -i '/Caper/d' all_the_names
-sed -i '/caper/d' all_the_names
-sed -i '/Soccer/d' all_the_names
-sed -i '/soccer/d' all_the_names
-sed -i '/Unpleasant/d' all_the_names
-sed -i '/unpleasant/d' all_the_names
-sed -i '/Unicycle/d' all_the_names
-sed -i '/unicycle/d' all_the_names
-sed -i '/Police/d' all_the_names
-sed -i '/police/d' all_the_names
-sed -i '/Horse Racing/d' all_the_names
-sed -i '/horse racing/d' all_the_names
-sed -i '/Drug-Dealing/d' all_the_names
-sed -i '/drug-dealing/d' all_the_names
-sed -i '/sonic/d' all_the_names
-sed -i '/Sonic/d' all_the_names
-sed -i '/Communist/d' all_the_names
-sed -i '/communist/d' all_the_names
-sed -i '/Brain-Damaged/d' all_the_names
-sed -i '/brain-damaged/d' all_the_names
-sed -i '/Janitor/d' all_the_names
-sed -i '/janitor/d' all_the_names
-sed -i '/Matador/d' all_the_names
-sed -i '/matador/d' all_the_names
-sed -i '/Little/d' all_the_names
-sed -i '/little/d' all_the_names
-sed -i '/Combat/d' all_the_names
-sed -i '/combat/d' all_the_names
-sed -i '/vs./d' all_the_names
-sed -i '/Badminton/d' all_the_names
-sed -i '/badminton/d' all_the_names
-sed -i '/Fighter/d' all_the_names
-sed -i '/fighter/d' all_the_names
-sed -i '/Nazi/d' all_the_names
-sed -i '/nazi/d' all_the_names
-sed -i '/Harvest/d' all_the_names
-sed -i '/harvest/d' all_the_names
-sed -i '/Wasteland/d' all_the_names
-sed -i '/wasteland/d' all_the_names
-sed -i '/Struggle/d' all_the_names
-sed -i '/struggle/d' all_the_names
-sed -i '/Vocabulary/d' all_the_names
-sed -i '/vocabulary/d' all_the_names
-# rm -f all_the_names1
+rm -f suffix prefix
 
-echo censored everything
+# check how many lines censor has keep only numbers and put it into variable liness
+censor_lines=`wc -l censor | sed 's/[^0-9]*//g'`
+
+
+
+# create multiple lines of sed -i '/ based on the number of lines
+
+# linux do command x times
+# https://serverfault.com/questions/273238/how-to-run-a-command-multiple-times-using-bash-shell
+for i in `seq $censor_lines`; do cat $PWD/lib/prefix >> prefix; done
+# sed -i '/
+# sed -i '/
+# sed -i '/
+
+for i in `seq $censor_lines`; do cat $PWD/lib/suffix >> suffix; done
+# /d' all_the_names
+# /d' all_the_names
+# /d' all_the_names
+
+# paste prefix to censor
+
+paste '-d\0' prefix censor > 1
+
+# paste created to suffix
+
+paste '-d\0' 1 suffix > 2
+
+sed -i '1i #!/bin/bash' 2
+
+sed -i '$a echo censored everything' 2
+
+chmod +x 2
+
+bash 2
+
+rm -f 1 2 suffix prefix
+
+rm -f suffix prefix
+
+echo done censoring censoring
 
---
+++ content/scripts/generate_name/lib/old_3_censorship.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+
+# rm -f all_the_names1
+
+# cp all_the_names all_the_names1
+
+# remove some names that match from the remove text file in the lib folder
+# grep -Fvxf $PWD/lib/remove all_the_names > all_the_names1
+
+# removes all the names that include words listed here
+sed -i '/emo/d' all_the_names
+sed -i '/Emo/d' all_the_names
+sed -i '/Teenage/d' all_the_names
+sed -i '/teenage/d' all_the_names
+sed -i '/angry/d' all_the_names
+sed -i '/Angry/d' all_the_names
+sed -i '/prison/d' all_the_names
+sed -i '/Prison/d' all_the_names
+sed -i '/Killer/d' all_the_names
+sed -i '/killer/d' all_the_names
+sed -i '/Kiler/d' all_the_names
+sed -i '/kiler/d' all_the_names
+sed -i '/boring/d' all_the_names
+sed -i '/Boring/d' all_the_names
+sed -i '/scam/d' all_the_names
+sed -i '/Scam/d' all_the_names
+sed -i '/Big/d' all_the_names
+sed -i '/big/d' all_the_names
+sed -i '/small/d' all_the_names
+sed -i '/Small/d' all_the_names
+sed -i '/assault/d' all_the_names
+sed -i '/Assault/d' all_the_names
+sed -i '/asault/d' all_the_names
+sed -i '/Asault/d' all_the_names
+sed -i '/Toxic/d' all_the_names
+sed -i '/toxic/d' all_the_names
+sed -i '/african/d' all_the_names
+sed -i '/African/d' all_the_names
+sed -i '/Australian/d' all_the_names
+sed -i '/australian/d' all_the_names
+sed -i '/indian/d' all_the_names
+sed -i '/Indian/d' all_the_names
+sed -i '/gun/d' all_the_names
+sed -i '/Gun/d' all_the_names
+sed -i '/rugby/d' all_the_names
+sed -i '/Rugby/d' all_the_names
+sed -i '/Unremarkable/d' all_the_names
+sed -i '/unremarkable/d' all_the_names
+sed -i '/Equestrian/d' all_the_names
+sed -i '/equestrian/d' all_the_names
+sed -i '/jew/d' all_the_names
+sed -i '/Jew/d' all_the_names
+sed -i '/Jewish/d' all_the_names
+sed -i '/jewish/d' all_the_names
+sed -i '/In Your Face/d' all_the_names
+sed -i '/Incomprehensible/d' all_the_names
+sed -i '/incomprehensible/d' all_the_names
+sed -i '/Murder/d' all_the_names
+sed -i '/murder/d' all_the_names
+sed -i '/hitman/d' all_the_names
+sed -i '/Hitman/d' all_the_names
+sed -i '/Shopping/d' all_the_names
+sed -i '/shopping/d' all_the_names
+sed -i '/Globetrotters/d' all_the_names
+sed -i '/globetrotters/d' all_the_names
+sed -i '/Booty/d' all_the_names
+sed -i '/booty/d' all_the_names
+sed -i '/Cheese/d' all_the_names
+sed -i '/cheese/d' all_the_names
+sed -i '/Dollar/d' all_the_names
+sed -i '/dollar/d' all_the_names
+sed -i '/Blimp/d' all_the_names
+sed -i '/blimp/d' all_the_names
+sed -i '/Slaughter/d' all_the_names
+sed -i '/slaughter/d' all_the_names
+sed -i '/Dating/d' all_the_names
+sed -i '/dating/d' all_the_names
+sed -i '/Rodeo/d' all_the_names
+sed -i '/rodeo/d' all_the_names
+sed -i '/Donkey/d' all_the_names
+sed -i '/donkey/d' all_the_names
+sed -i '/DS/d' all_the_names
+sed -i '/Lawnmower/d' all_the_names
+sed -i '/lawnmower/d' all_the_names
+sed -i '/Crossdressing/d' all_the_names
+sed -i '/crossdressing/d' all_the_names
+sed -i '/Caper/d' all_the_names
+sed -i '/caper/d' all_the_names
+sed -i '/Soccer/d' all_the_names
+sed -i '/soccer/d' all_the_names
+sed -i '/Unpleasant/d' all_the_names
+sed -i '/unpleasant/d' all_the_names
+sed -i '/Unicycle/d' all_the_names
+sed -i '/unicycle/d' all_the_names
+sed -i '/Police/d' all_the_names
+sed -i '/police/d' all_the_names
+sed -i '/Horse Racing/d' all_the_names
+sed -i '/horse racing/d' all_the_names
+sed -i '/Drug-Dealing/d' all_the_names
+sed -i '/drug-dealing/d' all_the_names
+sed -i '/sonic/d' all_the_names
+sed -i '/Sonic/d' all_the_names
+sed -i '/Communist/d' all_the_names
+sed -i '/communist/d' all_the_names
+sed -i '/Brain-Damaged/d' all_the_names
+sed -i '/brain-damaged/d' all_the_names
+sed -i '/Janitor/d' all_the_names
+sed -i '/janitor/d' all_the_names
+sed -i '/Matador/d' all_the_names
+sed -i '/matador/d' all_the_names
+sed -i '/Little/d' all_the_names
+sed -i '/little/d' all_the_names
+sed -i '/Combat/d' all_the_names
+sed -i '/combat/d' all_the_names
+sed -i '/vs./d' all_the_names
+sed -i '/Badminton/d' all_the_names
+sed -i '/badminton/d' all_the_names
+sed -i '/Fighter/d' all_the_names
+sed -i '/fighter/d' all_the_names
+sed -i '/Nazi/d' all_the_names
+sed -i '/nazi/d' all_the_names
+sed -i '/Harvest/d' all_the_names
+sed -i '/harvest/d' all_the_names
+sed -i '/Wasteland/d' all_the_names
+sed -i '/wasteland/d' all_the_names
+sed -i '/Struggle/d' all_the_names
+sed -i '/struggle/d' all_the_names
+sed -i '/Vocabulary/d' all_the_names
+sed -i '/vocabulary/d' all_the_names
+sed -i '/Exquisite/d' all_the_names
+sed -i '/exquisite/d' all_the_names
+# rm -f all_the_names1
+
+echo censored everything
+
