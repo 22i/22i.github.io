0b88ae893b079a5e16ae4b3042ca7d6d1b9a52ad
tri <pumpkin650@gmail.com>
2021-01-31 00:08:09 +0100

    better 404 page

---
+++ content/js/index_key_press.js
@@ -0,0 +1,94 @@
+
+
+
+// https://keycode.info/
+
+  
+
+
+
+  
+//   1
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 49) {
+     window.location.href = "#start";
+   }
+  });
+
+  
+    //   2
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 50) {
+     window.location.href = "#tools";
+   }
+  });
+  
+      //   3
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 51) {
+     window.location.href = "#programs";
+   }
+  });
+  
+        //   4
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 52) {
+     window.location.href = "#extensions";
+   }
+  });
+  
+          //   5
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 53) {
+     window.location.href = "#android9";
+   }
+  });
+  
+            //   6
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 54) {
+     window.location.href = "#tutorials";
+   }
+  });
+  
+              //   7
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 55) {
+     window.location.href = "#improve";
+   }
+  });
+  
+                //   8
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 56) {
+     window.location.href = "#questions";
+   }
+  });
+  
+                  //   9
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 57) {
+     window.location.href = "#clip";
+   }
+  });
+  
+                    //   0
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 48) {
+     window.location.href = "https://github.com/22i";
+   }
+  });
+  
+                      //   '
+  $(document).keydown(function(keyPressed) {
+   if (keyPressed.keyCode == 222) {
+     window.location.href = "http://1a-chillout.radionetz.de/1a-chillout.mp3";
+   }
+  });
+  
+  
+  
+
+  
+  
+  
--- content/js/key_press.js
+++ content/js/key_press.js
@@ -10,9 +10,9 @@
 
 //   if key up is pressed which is code 38 then change body background-color to tomato or moves you to top of the page
   
-  $(document).keydown(function(keyPressed) {
-   if (keyPressed.keyCode == 38) {
+//   $(document).keydown(function(keyPressed) {
+//    if (keyPressed.keyCode == 38) {
 //      $("body").css("background-color","tomato");
-     window.location.href = "#top";
-   }
-  });
+//      window.location.href = "#top";
+//    }
+//   });
