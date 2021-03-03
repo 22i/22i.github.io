ceb176c7bb73df1619b696126cd0820131d7ebb4
22i <pumpkin650@gmail.com>
2021-03-03 11:38:47 +0100

    update

---
+++ content/scripts/get_requirements.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+
+sudo apt install git
+sudo apt install xdotool
+sudo apt install mpv
+sudo apt install wkhtmltopdf
+sudo apt install rsync
+sudo apt install yad
+sudo apt install ffmpegthumbs
+sudo apt install xclip
+
+
+echo done everything
