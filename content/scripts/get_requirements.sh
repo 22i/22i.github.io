ead2378353c98679e359ace8fc2ff549161d8ec6
22i <pumpkin650@gmail.com>
2021-03-04 09:35:23 +0100

    update

--- content/scripts/get_requirements.sh
+++ content/scripts/get_requirements.sh
@@ -1,15 +1,64 @@
 #!/bin/bash
 
+# add echo git to get_requirements add auto yes
 
-
+echo git
 sudo apt install git
+echo xdotool
 sudo apt install xdotool
+echo mpv
 sudo apt install mpv
+echo wkhtmltopdf
 sudo apt install wkhtmltopdf
+echo wkhtmltoimage
+sudo apt install wkhtmltoimage
+echo rsync
 sudo apt install rsync
+echo yad
 sudo apt install yad
+echo ffmpegthumbs
 sudo apt install ffmpegthumbs
+echo xclip
 sudo apt install xclip
+echo thunderbird
+sudo apt install thunderbird
+echo lynx
+sudo apt install lynx
+echo audacity
+sudo apt install audacity
+echo qbittorrent
+sudo apt install qbittorrent
+echo ffmpeg
+sudo apt install ffmpeg
+echo pdftk
+sudo apt install pdftk
+echo pdftoppm
+sudo apt install pdftoppm
+echo imagemagick
+sudo apt install imagemagick
+echo xournal
+sudo apt install xournal
+echo gimp
+sudo apt install gimp
+echo gparted
+sudo apt install gparted
+echo lm-sensors
+sudo apt install lm-sensors
+echo xosd-bin
+sudo apt install xosd-bin
+echo python3
+sudo apt install python3
+echo python
+sudo apt install python
+echo youtube-dl
+# sudo apt install youtube-dl
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && sudo chmod a+rx /usr/local/bin/youtube-dl
+sudo apt install libvorbis
+# sudo apt install HandBrakeCLI
+# sudo apt install mkvtoolnix
+echo plasma-sdk
+sudo apt install plasma-sdk
+
 
 
 echo done everything
