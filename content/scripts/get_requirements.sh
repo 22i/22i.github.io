87e04eaf88a95ced14c40b171dd72e39f4c607ce
22i <pumpkin650@gmail.com>
2021-03-08 08:13:37 +0100

    update

--- content/scripts/get_requirements.sh
+++ content/scripts/get_requirements.sh
@@ -3,62 +3,77 @@
 # add echo git to get_requirements add auto yes
 
 echo git
-sudo apt install git
+sudo apt install git -y
 echo xdotool
-sudo apt install xdotool
+sudo apt install xdotool -y
 echo mpv
-sudo apt install mpv
+sudo apt install mpv -y
 echo wkhtmltopdf
-sudo apt install wkhtmltopdf
+sudo apt install wkhtmltopdf -y
 echo wkhtmltoimage
-sudo apt install wkhtmltoimage
+sudo apt install wkhtmltoimage -y
 echo rsync
-sudo apt install rsync
+sudo apt install rsync -y
 echo yad
-sudo apt install yad
+sudo apt install yad -y
 echo ffmpegthumbs
-sudo apt install ffmpegthumbs
+sudo apt install ffmpegthumbs -y
 echo xclip
-sudo apt install xclip
+sudo apt install xclip -y
 echo thunderbird
-sudo apt install thunderbird
+sudo apt install thunderbird -y
 echo lynx
-sudo apt install lynx
+sudo apt install lynx -y
 echo audacity
-sudo apt install audacity
+sudo apt install audacity -y
 echo qbittorrent
-sudo apt install qbittorrent
+sudo apt install qbittorrent -y
 echo ffmpeg
-sudo apt install ffmpeg
+sudo apt install ffmpeg -y
 echo pdftk
-sudo apt install pdftk
+sudo apt install pdftk -y
 echo pdftoppm
-sudo apt install pdftoppm
+sudo apt install pdftoppm -y
 echo imagemagick
-sudo apt install imagemagick
+sudo apt install imagemagick -y
 echo xournal
-sudo apt install xournal
+sudo apt install xournal -y
 echo gimp
-sudo apt install gimp
+sudo apt install gimp -y
 echo gparted
-sudo apt install gparted
+sudo apt install gparted -y
 echo lm-sensors
-sudo apt install lm-sensors
+sudo apt install lm-sensors -y
 echo xosd-bin
-sudo apt install xosd-bin
+sudo apt install xosd-bin -y
 echo python3
-sudo apt install python3
+sudo apt install python3 -y
 echo python
-sudo apt install python
+sudo apt install python -y
 echo youtube-dl
-# sudo apt install youtube-dl
+# sudo apt install youtube-dl -y
 sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl && sudo chmod a+rx /usr/local/bin/youtube-dl
-sudo apt install libvorbis
-# sudo apt install HandBrakeCLI
-# sudo apt install mkvtoolnix
+sudo apt install libvorbis -y
+# sudo apt install HandBrakeCLI -y
+# sudo apt install mkvtoolnix -y
 echo plasma-sdk
-sudo apt install plasma-sdk
-
-
+sudo apt install plasma-sdk -y
+# https://gitlab.com/torkel104/libstrangle
+# limit program, game fps
+# usage:
+# strangle 30 /path/to/game
+echo libstrangle
+sudo apt install gcc-multilib -y
+sudo apt install g++-multilib -y
+sudo apt install libx11-dev -y
+sudo apt install mesa-common-dev -y
+cd $PWD/temp/
+git clone https://gitlab.com/torkel104/libstrangle.git
+cd $PWD/libstrangle/
+make
+sudo make install
+cd ..
+cd ..
+rm -f -r $PWD/temp/*
 
 echo done everything
