9bf0aeb68a6e93f35b32cb5853789f04c19e9e59
22i <pumpkin650@gmail.com>
2022-11-07 15:56:04 +0100

    added instructions for yt-dlp on termux

---
+++ content/scripts/termux/get.video.termux.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# go into phone storage Download folder
+cd /storage/emulated/0/Download/
+
+# download youtube video
+yt-dlp https://www.youtube.com/watch?v=H8uFV2Useho
+
+# show message of completion when done
+echo done everything
