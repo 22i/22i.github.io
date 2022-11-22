b68e0922d3d429f60ec032ee3d0b325a97c80c79
22i <pumpkin650@gmail.com>
2022-11-22 21:33:17 +0100

    update

--- content/scripts/termux/get.video.termux.sh
+++ content/scripts/termux/get.video.termux.sh
@@ -3,8 +3,11 @@
 # go into phone storage Download folder
 cd /storage/emulated/0/Download/
 
-# download youtube video
-yt-dlp https://www.youtube.com/watch?v=H8uFV2Useho
+# download youtube video with subtitles
+yt-dlp --write-subs https://www.youtube.com/watch?v=H8uFV2Useho
+
+# download youtube video at defined rate to not slow down the swarm
+# yt-dlp --write-subs --limit-rate 200K https://www.youtube.com/watch?v=H8uFV2Useho
 
 # show message of completion when done
 echo done everything
